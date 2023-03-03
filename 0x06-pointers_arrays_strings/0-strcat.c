b887bb0a581ce833a2f192c8915b4220b43a8d98
Kabura7 <kaburaian@gmail.com>
2023-03-03 11:07:02 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *_strcat - appends the src string to the dest
+ *string
+ *
+ *@dest: a string pointer
+ *@src:  a string
+ *
+ *Return: Returns a poiter to the resulting string.
+ */
