fafe47e5ed16d22bc6fe6c61645f56b4b6de0431
Kabura7 <kaburaian@gmail.com>
2023-03-10 23:53:55 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - accepts the arguments
+ * @argc:count of number of arguments
+ * @argv:pointer to array of strings
+ * Return: always 0
+ */
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
