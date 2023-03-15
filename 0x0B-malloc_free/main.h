360a333479c7970375a3bc42653b28339461e4ab
Kabura7 <kaburaian@gmail.com>
2023-03-15 12:49:53 -0700

    100-argstostr.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int _putchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
